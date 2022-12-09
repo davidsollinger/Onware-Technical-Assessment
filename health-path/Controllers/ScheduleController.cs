7ba7f446ee2d266c03b7616f7d17e3f5bc9c625f
David Sollinger <davidsollinger@users.noreply.github.com>
2022-12-09 05:19:47 +0000

    Attempted to fix issue regarding duplicating event names in class schedule database.

--- health-path/Controllers/ScheduleController.cs
+++ health-path/Controllers/ScheduleController.cs
@@ -23,6 +23,9 @@ public class ScheduleController : ControllerBase
     {
         var dbResults = ReadData();
 
+        //attempted this strategy to group the items together but it did not work
+        //dbResults.GroupBy(o => o.Item1).Select(g => g.Skip(1).Aggregate(g.First(), (a, o) => {a.Item2.StartTime += o.Item2.StartTime; a.Item2.EndTime += o.Item2.EndTime; return a;}));
+
         var preparedResults = dbResults.Select((t) => {
             t.Item1.Recurrences.Add(t.Item2);
             return t.Item1;
@@ -31,13 +34,17 @@ public class ScheduleController : ControllerBase
         return Ok(preparedResults);
     }
 
-    private IEnumerable<(ScheduleEvent, ScheduleEventRecurrence)> ReadData() {
+    private IEnumerable<(ScheduleEvent, ScheduleEventRecurrence)> ReadData() 
+    {
+        //change to combine two rows with the same EventId
+        //GROUP BY was not working
         var sql = @"
             SELECT e.*, r.*
             FROM Event e
             JOIN EventRecurrence r ON e.Id = r.EventId
             ORDER BY e.Id, r.DayOfWeek, r.StartTime, r.EndTime
         ";
+        
         return _connection.Query<ScheduleEvent, ScheduleEventRecurrence, (ScheduleEvent, ScheduleEventRecurrence)>(sql, (e, r) => (e, r));
     }
 }
